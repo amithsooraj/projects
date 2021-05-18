851f2be7ddde58bac311aa14040465292920fe49
Sarath.M <sarathm00@gmail.com>
2021-05-18 12:40:31 +0530

    First commit

---
+++ prblm1/q1.py
@@ -0,0 +1,45 @@
+from datetime import datetime as dt
+
+
+def parse_file(fname):
+    '''
+    fname: path to the input file
+    returns: dictionary containing name, age pairs
+    '''
+    cntr = 1
+    names, ages = [], []
+    with open(fname, 'r') as fl:
+        for line in fl:
+            line = line.strip()
+            if cntr % 2 != 0:
+                names.append(line)
+            else:
+                ages.append(line)
+            # increment line counter
+            cntr += 1
+    data = dict(zip(names, ages))
+    return data
+
+
+def sort_dict(data):
+    '''
+    dict: Dictionary containing name, age
+    return sorted dictionary
+    '''
+    sorted_data = dict(
+        sorted(data.items(),
+               key=lambda item: dt.strptime(item[1], '%d-%m-%Y'),
+               reverse=True),
+    )
+    new_data = {}
+    for key, value in data.items():
+        new_data['name'] = key
+        new_data['age'] = dt.strptime(value, '%d-%m-%Y')
+    return sorted_data
+
+
+if __name__ == '__main__':
+    data = parse_file('ag1.txt')
+    print(data, '\n\n')
+    sorted_data = sort_dict(data)
+    print(sorted_data, '\n\n')
